3ca9e9837cfbd1049aa6cde65eb4797f6246abfd
DaviLeocadio <davi.leocadio.sen@gmail.com>
2025-08-06 16:43:03 -0300

    Alterações 06/08 Senai

--- frontend/src/app/user/meus_chamados/page.jsx
+++ frontend/src/app/user/meus_chamados/page.jsx
@@ -5,7 +5,15 @@ import Card from "@/components/card/card";
 export default function Meus_chamados() {
   return (
     <>
-      
+      <Image 
+      src={'/fundo.jpeg'}
+      height={200}
+      width={400}
+      alt=""
+      className="fundo img-fluid"
+      />
+
+      <h2>Meus Chamados</h2>
       <div className="cards d-flex gap-4 flex-wrap">
       <Card />
      
